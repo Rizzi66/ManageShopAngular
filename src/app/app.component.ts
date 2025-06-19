053855de6c447cb937c1a509133a5cceb7543ffd
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-06-19 12:03:20 +0200

    Déplacement des fichiers de base de l'app Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ManageShopAngular';
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AppInitializerService } from './core/services/app-initializer.service';
+import { TokenInterceptor } from './core/interceptors/token.interceptor';
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+export function initializeApp(appInitializer: AppInitializerService) {
+  return async () => {
+    try {
+      await appInitializer.initializeApp();
+    } catch (error) {
+      console.error("Erreur pendant l'initialisation", error);
+      throw error;
+    }
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
+    provideRouter(APP_ROUTES),
+    provideHttpClient(withInterceptors([TokenInterceptor])),
+    AppInitializerService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [AppInitializerService],
+      multi: true,
+    },
   ],
 };
