1595ffe9c6641700444e61587da89dd9a00cae23
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-06-19 12:17:41 +0200

    Création composant pour "super-admin"

---
+++ src/app/features/super-admin/pages/stores-management/stores-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stores-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './stores-management.component.html',
+  styleUrl: './stores-management.component.scss',
+})
+export class StoresManagementComponent {}
